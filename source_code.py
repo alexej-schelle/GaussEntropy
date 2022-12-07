6d0309ee4f240ad197638b21f1a692f91b977752
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2022-12-07 14:49:38 +0100

    Create source_code.py

---
+++ source_code.py
@@ -0,0 +1,5 @@
+# The source code is available at Dr. A. Schelle, Freelancer Lecturer @ IU International University of Applied Sciences.
+#
+# or
+#
+# Hannes Lüling, CEO and Founder @ hema.to GmbH, Ainmiller Str. 22, 80801 München. 
